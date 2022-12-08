38a1825d685727d428156ebcd18de8628fac53b7
edgar <edgaromarsf@gmail.com>
2022-12-07 18:33:02 -0600

    cambios en vista de admin

--- tribunal/admin.py
+++ tribunal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Tribunal
 
-
-admin.site.register(Tribunal)
+@admin.register(Tribunal)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'ubicacion', 'estado']
