a28ea368b946a08116604017ec1ef7a5e06c75c0
Edgar Omar Sanchez Farias <edgaromarsf@gmail.com>
2024-08-28 17:54:06 -0600

    vercel config

--- resumendeproductividad_site/wsgi.py
+++ resumendeproductividad_site/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resumendeproductividad_site.settings')
 
 application = get_wsgi_application()
+
+app = application
