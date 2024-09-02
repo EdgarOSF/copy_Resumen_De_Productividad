0b730869778d6d85d9ba575a16dacf4f973ac17c
Edgar Omar Sanchez Farias <edgaromarsf@gmail.com>
2024-09-02 17:29:02 -0600

    change build file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python311 -m pip install -r requirements.txt
+python311 manage.py collectstatic --noinput --clear
 echo "BUILD END"
