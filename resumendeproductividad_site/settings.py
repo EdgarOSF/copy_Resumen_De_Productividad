330b8e84ece431e96f3614c80e7861cf610dfc1e
Edgar Omar Sanchez Farias <edgaromarsf@gmail.com>
2024-09-02 17:28:29 -0600

    debug false

--- resumendeproductividad_site/settings.py
+++ resumendeproductividad_site/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG_IS_ACTIVATE', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
