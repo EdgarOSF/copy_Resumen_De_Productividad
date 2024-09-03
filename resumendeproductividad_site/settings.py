bae508384516c414b90a0a43c00e38c36927c42f
EdgarOSF <edgaromarsf@gmail.com>
2024-09-02 18:31:34 -0600

    Update settings.py

--- resumendeproductividad_site/settings.py
+++ resumendeproductividad_site/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = False
 
 DEBUG = config('DEBUG_IS_ACTIVATE', default=False, cast=bool)
 
