d48f00a52dd015462fe1bae57b180169e7aa64c1
EdgarOSF <edgaromarsf@gmail.com>
2024-09-02 18:32:15 -0600

    Update settings.py

--- resumendeproductividad_site/settings.py
+++ resumendeproductividad_site/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = False
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG_IS_ACTIVATE', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
